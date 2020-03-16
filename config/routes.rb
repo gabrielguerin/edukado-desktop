5b4d303cd7d7954be1aff72da8a09c61284e1c6f
Gabriel Guérin <gabriel.gueringg@gmail.com>
2020-03-16 20:45:43 +0100

    translations & routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,85 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   namespace :admin do
-      resources :users
-      resources :blogs
-      resources :comments
-      resources :faqs
-      resources :posts
-      resources :statics
-      resources :tags
+    resources :users
 
-      root to: "users#index"
+    resources :posts
+
+    resources :comments
+
+    resources :tags
+
+    resources :blogs, path: 'blog'
+
+    resources :faqs, path: 'faq'
+
+    resources :posts_tags
+
+    resources :tags_users
+
+    namespace :merit do
+      resources :activity_logs
+
+      resources :badges_sashes
+
+      resources :sashes
+
+      resources :scores
+
+      namespace :score do
+        resources :points
+      end
     end
-  resources :faqs
-  resources :blogs
-  resources :statics
-  resources :tags
-  resources :comments
-  resources :posts
-  devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+    namespace :acts_as_votable do
+      resources :votes
+    end
+
+    namespace :pg_search do
+      resources :documents
+    end
+
+    root to: 'users#index'
+  end
+
+  root 'statics#show', page: 'index'
+
+  resources :faqs, only: %i[index], path: 'faq'
+
+  resources :blogs, only: %i[show index], path: 'blog'
+
+  resources :tags, only: %i[show]
+
+  devise_for :users, path: 'users', controllers: {
+
+    registrations: 'users/registrations',
+
+    invitations: 'users/invitations',
+
+    confirmations: 'users/confirmations'
+
+  }
+
+  resources :users do
+    member do
+      get :posts
+    end
+  end
+
+  resources :posts do
+    resources :comments do
+      member do
+        get 'like'
+
+        get 'dislike'
+      end
+    end
+
+    member do
+      get 'like'
+
+      get 'dislike'
+    end
+  end
 end
