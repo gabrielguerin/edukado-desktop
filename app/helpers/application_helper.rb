ab7b1435d01a96e9cd71de40f877a4e75fcbbc37
Gabriel Guérin <gabriel.gueringg@gmail.com>
2020-03-17 20:41:19 +0100

    user avatar helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,15 @@
+# frozen_string_literal: true
+
 module ApplicationHelper
   def app_name
     Rails.application.class.module_parent_name
+  end
+
+  def user_avatar(user)
+    if user.avatar.attached?
+      user.avatar
+    else
+      image_url('fallback/default')
+    end
   end
 end
