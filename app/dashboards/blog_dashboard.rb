b0a8fb99313439099f4efea996fb87e97af42558
Gabriel Guérin <gabriel.gueringg@gmail.com>
2020-03-18 20:57:29 +0100

    profile pictures and dashboard working everywhere

--- app/dashboards/blog_dashboard.rb
+++ app/dashboards/blog_dashboard.rb
@@ -1,4 +1,6 @@
-require "administrate/base_dashboard"
+# frozen_string_literal: true
+
+require 'administrate/base_dashboard'
 
 class BlogDashboard < Administrate::BaseDashboard
   # ATTRIBUTE_TYPES
@@ -14,10 +16,11 @@ class BlogDashboard < Administrate::BaseDashboard
     tags: Field::HasMany,
     id: Field::Number,
     title: Field::String,
+    body: RichTextAreaField,
     description: Field::String,
     created_at: Field::DateTime,
     updated_at: Field::DateTime,
-    slug: Field::String,
+    slug: Field::String
   }.freeze
 
   # COLLECTION_ATTRIBUTES
@@ -26,38 +29,39 @@ class BlogDashboard < Administrate::BaseDashboard
   # By default, it's limited to four items to reduce clutter on index pages.
   # Feel free to add, remove, or rearrange items.
   COLLECTION_ATTRIBUTES = %i[
-  rich_text_body
-  user
-  blogs_tags
-  tags
+    id
+    title
+    description
+    tags
+    created_at
   ].freeze
 
   # SHOW_PAGE_ATTRIBUTES
   # an array of attributes that will be displayed on the model's show page.
   SHOW_PAGE_ATTRIBUTES = %i[
-  rich_text_body
-  user
-  blogs_tags
-  tags
-  id
-  title
-  description
-  created_at
-  updated_at
-  slug
+    user
+    blogs_tags
+    body
+    tags
+    id
+    title
+    description
+    created_at
+    updated_at
+    slug
   ].freeze
 
   # FORM_ATTRIBUTES
   # an array of attributes that will be displayed
   # on the model's form (`new` and `edit`) pages.
   FORM_ATTRIBUTES = %i[
-  rich_text_body
-  user
-  blogs_tags
-  tags
-  title
-  description
-  slug
+    body
+    user
+    blogs_tags
+    tags
+    title
+    description
+    slug
   ].freeze
 
   # COLLECTION_FILTERS
