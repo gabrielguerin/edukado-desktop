0157e239e059f601fd55532d20bba76d2510c3e5
Gabriel Guérin <gabriel.gueringg@gmail.com>
2020-03-20 18:05:40 +0100

    added comments

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,6 +17,7 @@ class UsersController < ApplicationController
 
   private
 
+  # Use callbacks to share common setup or constraints between actions.
   def set_user
     @user = User.friendly.find(params[:id])
   end
