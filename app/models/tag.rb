18d8f93c1a62d9fb222f3ad0982b579c3c3c086a
Gabriel Guérin <gabriel.gueringg@gmail.com>
2020-03-20 17:45:21 +0100

    removed useless space

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -2,26 +2,15 @@
 
 class Tag < ApplicationRecord
   extend FriendlyId
-
   friendly_id :title, use: :slugged
-
   has_many :posts_tags, dependent: :destroy
-
   has_many :posts, through: :posts_tags
-
   has_many :blogs_tags, dependent: :destroy
-
   has_many :blogs, through: :blogs_tags
-
   has_many :tags_users, dependent: :destroy
-
   has_many :users, through: :tags_users
-
   validates :title, presence: true, length: {
-
     minimum: 2,
-
     too_short: '%<count> caractères est le minimum autorisé'
-
   }
 end
