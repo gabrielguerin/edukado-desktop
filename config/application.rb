60717dbef17378c5c2f525fa87cb923c5de869e1
Gabriel Guérin <gabriel.gueringg@gmail.com>
2020-03-21 15:26:38 +0100

    avoided hardcoding postgresql username & password

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,14 @@ module EdukadoDesktop
     config.to_prepare do
       Administrate::ApplicationController.helper EdukadoDesktop::Application.helpers
     end
+
+    config.before_configuration do
+      env_file = File.join(Rails.root, 'config', 'local_env.yml')
+      if File.exist?(env_file)
+        YAML.safe_load(File.open(env_file)).each do |key, value|
+          ENV[key.to_s] = value
+        end
+      end
+    end
   end
 end
