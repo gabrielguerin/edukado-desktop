77afc9e3ba0c9092fb680587f028d566a2311183
Gabriel Guérin <gabriel.gueringg@gmail.com>
2020-03-30 00:14:53 +0200

    nicer admin dashboard & non-admin users cannot access to the admin dashboard

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -1,32 +1,37 @@
 # frozen_string_literal: true
 
 # All Administrate controllers inherit from this `Admin::ApplicationController`,
+
 # making it the ideal place to put authentication logic or other
+
 # before_actions.
-#
+
 # If you want to add pagination or other controller-level concerns,
+
 # you're free to overwrite the RESTful controller actions.
+
 module Admin
   class ApplicationController < Administrate::ApplicationController
     layout 'sidenav'
 
     before_action :authenticate_user!
+
     before_action :authenticate_admin
 
     def authenticate_admin
-      redirect_to '/', alert: 'Not authorized.' unless current_user && access?
-    end
-
-    private
-
-    def access?
-      current_user.admin = true
+      unless current_user&.admin?
+        redirect_to '/', alert: "Vous n'êtes pas autorisé à accéder à cette page."
+      end
     end
 
     # Override this value to specify the number of elements to display at a time
+
     # on index pages. Defaults to 20.
+
     # def records_per_page
+
     #   params[:per_page] || 20
+
     # end
   end
 end
