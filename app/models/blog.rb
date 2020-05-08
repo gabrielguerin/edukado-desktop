20283a16329dd03941ce37920af2d4f8ff638383
Gabriel Guérin <gabriel.gueringg@gmail.com>
2020-05-08 15:29:58 +0200

    deleted pgsearchmodel from blog

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,13 +1,24 @@
 # frozen_string_literal: true
 
+
+
 class Blog < ApplicationRecord
+
   has_rich_text :body
+
   has_one_attached :cover
+
   extend FriendlyId
+
   friendly_id :title, use: :slugged
-  include PgSearch::Model
+
+
 
   belongs_to :user
+
   has_many :blogs_tags, dependent: :destroy
+
   has_many :tags, through: :blogs_tags
+
 end
+
