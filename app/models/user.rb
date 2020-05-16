b0433fa388e1ebb656bf2e2a6c2ce7598f79556e
Gabriel Guérin <gabriel.gueringg@gmail.com>
2020-05-16 21:29:22 +0200

    When user has no notification, display it

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,140 +1,70 @@
 # frozen_string_literal: true
 
-
-
 class User < ApplicationRecord
-
   # Badges
-
-
 
   has_merit
 
-
-
   # FriendlyId
-
-
 
   extend FriendlyId
 
-
-
   friendly_id :full_name, use: :slugged
-
-
 
   # Active Storage
 
-
-
   has_one_attached :avatar
-
-
 
   # Vote
 
-
-
   acts_as_voter
 
-
-
   # Send welcome email
-
-
 
   after_commit :welcome_send
 
-
-
   # Devise modules
 
-
-
   devise :invitable, :database_authenticatable, :registerable,
-
          :recoverable, :rememberable, :validatable, :confirmable
-
-
 
   # Associations
 
-
-
   has_many :tags_users, dependent: :destroy
-
-
 
   has_many :tags, through: :tags_users
 
-
-
   has_many :posts
-
-
 
   has_many :comments
 
-
-
   has_many :blogs
-
-
 
   has_many :invitations, class_name: to_s, as: :invited_by
 
-
-
   has_many :notifications, dependent: :destroy
-
-
 
   # Validations
 
-
-
   validates :description, length: {
-
-
 
     maximum: 550,
 
-
-
     too_long: '%<count> caractères est le maximum autorisé'
-
-
 
   }, presence: false
 
-
-
   # Full name
 
-
-
   def full_name
-
     "#{first_name} #{last_name}"
-
   end
-
-
 
   private
 
-
-
   # Send welcome email
 
-
-
   def welcome_send
-
     UserMailer.welcome_email(self).deliver_now
-
   end
-
 end
-
