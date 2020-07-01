d1a5c3a6b0aa92476b50b47e356d15525dee09e3
Gabriel Guérin <gabriel.gueringg@gmail.com>
2020-07-01 18:17:07 +0200

    Changed users' side panel & groups#show

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -48,13 +48,13 @@ class GroupsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_group
-      @group = Group.friendly.find(params[:id])
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_group
+    @group = Group.friendly.find(params[:id])
+  end
 
-    # Only allow a trusted parameter "white list" through.
-    def group_params
-      params.require(:group).permit(:name)
-    end
+  # Only allow a trusted parameter "white list" through.
+  def group_params
+    params.require(:group).permit(:name)
+  end
 end
--- app/models/group.rb
+++ app/models/group.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Group < ApplicationRecord
-  # Friendly id
+  # FriendlyId
 
   extend FriendlyId
 
