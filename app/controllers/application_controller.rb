e099cbc5a199cd45038a4f76529cfb0317cef91f
Gabriel Guérin <gabriel.gueringg@gmail.com>
2020-10-09 17:13:48 +0100

    Added pundit to application

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,12 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  # Authorization system
+
+  include Pundit
+
   # Set default url for Devise Mailer based on environment
+
   def default_url_options
     { host: ENV['DOMAIN'] || 'localhost:3000' }
   end
