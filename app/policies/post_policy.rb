e049a43e85856a9efec917dfa09d45a4862cf5ff
Gabriel Guérin <gabriel.gueringg@gmail.com>
2020-10-09 17:44:18 +0100

    Added pundit gem

--- app/policies/post_policy.rb
+++ app/policies/post_policy.rb
@@ -4,4 +4,21 @@ class PostPolicy < ApplicationPolicy
       scope.all
     end
   end
+
+  def new? ; user_is_owner_of_record? ; end
+  def create? ; user_is_owner_of_record? ; end
+
+  def show?
+    user_is_owner_of_record? || @record.published?
+  end
+
+  def update? ; user_is_owner_of_record? ; end
+  def destroy? ; user_is_owner_of_record? ; end
+
+  private
+
+  def user_is_owner_of_record?
+    @user == @record.user
+  end
+
 end
