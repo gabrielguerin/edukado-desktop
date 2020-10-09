97df2b7a016cfb45b341bb280938362b18a456a6
Gabriel Guérin <gabriel.gueringg@gmail.com>
2020-10-09 17:54:25 +0100

    Add application controller pundit setup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,9 +5,19 @@ class ApplicationController < ActionController::Base
 
   include Pundit
 
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
   # Set default url for Devise Mailer based on environment
 
   def default_url_options
     { host: ENV['DOMAIN'] || 'localhost:3000' }
+  end
+
+  private
+
+  def user_not_authorized
+    flash[:alert] = "Vous n'êtes pas autorisés à accéder à cette page"
+
+    redirect_to request.referrer || root_path
   end
 end
