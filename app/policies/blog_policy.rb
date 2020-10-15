0dcb48d63253ae559b1c5c0ba45c6e5e244efacc
Gabriel Guérin <gabriel.gueringg@gmail.com>
2020-10-15 17:01:37 +0100

    Added policies for blog

--- app/policies/blog_policy.rb
+++ app/policies/blog_policy.rb
@@ -1,7 +1,39 @@
+# frozen_string_literal: true
+
 class BlogPolicy < ApplicationPolicy
   class Scope < Scope
     def resolve
       scope.all
     end
+  end
+
+  def index?; end
+
+  def show?
+    true
+  end
+
+  def new?; end
+
+  def edit?
+    superadmin?
+  end
+
+  def create?
+    superadmin?
+  end
+
+  def update?
+    superadmin?
+  end
+
+  def destroy?
+    superadmin?
+  end
+
+  private
+
+  def superadmin?
+    @user&.superadmin_role?
   end
 end
