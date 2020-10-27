df0a5a756fc2525219be6cb1a618f8d51f20e75c
Gabriel Guérin <gabriel.gueringg@gmail.com>
2020-10-27 16:54:49 +0000

    Modify course model

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,7 +1,29 @@
+# frozen_string_literal: true
+
 class Course < ApplicationRecord
+  # Search
+
+  searchkick word_start: %i[name]
+
+  # FriendlyId
+
+  extend FriendlyId
+
+  friendly_id :name, use: :slugged
+
   # Associations
 
   belongs_to :group
 
   belongs_to :subject
+
+  # Search data
+
+  def search_data
+    {
+
+      name: name
+
+    }
+  end
 end
--- app/models/group.rb
+++ app/models/group.rb
@@ -1,66 +1,33 @@
 # frozen_string_literal: true
 
-
-
 class Group < ApplicationRecord
-
   # Search
-
-
 
   searchkick word_start: %i[name]
 
-
-
   # FriendlyId
-
-
 
   extend FriendlyId
 
-
-
   friendly_id :name, use: :slugged
-
-
 
   # Associations
 
-
-
   has_many :users
-
-
 
   has_many :posts
 
-
-
   has_many :courses, dependent: :destroy
-
-
 
   has_many :subjects, through: :courses
 
-
-
   # Search data
 
-
-
   def search_data
-
     {
-
-
 
       name: name
 
-
-
     }
-
   end
-
 end
-
