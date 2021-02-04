bc96992be99c2845954dd41eed244b69387f7cb7
Gabriel Guérin <gabriel.gueringg@gmail.com>
2021-02-04 18:12:38 +0000

    Dropzone translations

--- app/javascript/controllers/dropzone_controller.js
+++ app/javascript/controllers/dropzone_controller.js
@@ -160,13 +160,13 @@ function createDropZone(controller) {
     });
 }
 
-Dropzone.prototype.defaultOptions.dictDefaultMessage = "Drop files here to upload";
-Dropzone.prototype.defaultOptions.dictFallbackMessage = "Your browser does not support drag'n'drop file uploads.";
-Dropzone.prototype.defaultOptions.dictFallbackText = "Please use the fallback form below to upload your files like in the olden days.";
-Dropzone.prototype.defaultOptions.dictFileTooBig = "File is too big ({{filesize}}MiB). Max filesize: {{maxFilesize}}MiB.";
-Dropzone.prototype.defaultOptions.dictInvalidFileType = "You can't upload files of this type.";
-Dropzone.prototype.defaultOptions.dictResponseError = "Server responded with {{statusCode}} code.";
-Dropzone.prototype.defaultOptions.dictCancelUpload = "Cancel upload";
-Dropzone.prototype.defaultOptions.dictCancelUploadConfirmation = "Are you sure you want to cancel this upload?";
-Dropzone.prototype.defaultOptions.dictRemoveFile = "Remove file";
-Dropzone.prototype.defaultOptions.dictMaxFilesExceeded = "You can not upload any more files.";
+Dropzone.prototype.defaultOptions.dictDefaultMessage = "Déposez vos fichiers ici";
+Dropzone.prototype.defaultOptions.dictFallbackMessage = "Votre navigateur ne vous permet pas de télécharger vos fichiers en les déposant.";
+Dropzone.prototype.defaultOptions.dictFallbackText = "Veuillez utiliser le formulaire suivant pour télécharger vos fichiers comme au bon vieux temps.";
+Dropzone.prototype.defaultOptions.dictFileTooBig = "Le fichier est trop volumineux ({{filesize}}}Mo). Taille maximale : {{maxFilesize}}Mo.";
+Dropzone.prototype.defaultOptions.dictInvalidFileType = "Vous ne pouvez pas télécharger des fichiers de ce type.";
+Dropzone.prototype.defaultOptions.dictResponseError = "Le serveur a répondu avec le code {{statusCode}}.";
+Dropzone.prototype.defaultOptions.dictCancelUpload = "Annuler le téléchargement";
+Dropzone.prototype.defaultOptions.dictCancelUploadConfirmation = "Êtes-vous sûr de vouloir annuler ce téléchargement ?";
+Dropzone.prototype.defaultOptions.dictRemoveFile = "Supprimer le fichier";
+Dropzone.prototype.defaultOptions.dictMaxFilesExceeded = "Vous ne pouvez plus télécharger de fichiers.";
