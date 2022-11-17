405d16c0f7d27f49ee843e014afd5470b277cda6
Gabriel GUERIN <gabriel.gueringg@gmail.com>
2022-11-17 15:34:32 +0100

    Comment Sendgrid credentials

--- config/environment.rb
+++ config/environment.rb
@@ -8,22 +8,22 @@ require_relative 'application'
 
 Rails.application.initialize!
 
-# Sendgrid
+# # Sendgrid
 
-ActionMailer::Base.smtp_settings = {
+# ActionMailer::Base.smtp_settings = {
 
-  user_name: Rails.application.credentials.sendgrid[:user_name],
+#   user_name: Rails.application.credentials.sendgrid[:user_name],
 
-  password: Rails.application.credentials.sendgrid[:password],
+#   password: Rails.application.credentials.sendgrid[:password],
 
-  domain: 'herokuapp.com',
+#   domain: 'herokuapp.com',
 
-  address: 'smtp.sendgrid.net',
+#   address: 'smtp.sendgrid.net',
 
-  port: 587,
+#   port: 587,
 
-  authentication: :plain,
+#   authentication: :plain,
 
-  enable_starttls_auto: true
+#   enable_starttls_auto: true
 
-}
+# }
